ac83a9c9dcdcac1c3037e72c610824187d791c11
matheswaaran <matheswaarans@gmail.com>
2017-03-21 09:35:54 +0530

    home

--- ProjectIHRM/Program.cs
+++ ProjectIHRM/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectIHRM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
